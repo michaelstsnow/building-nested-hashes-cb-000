19fc12f46b2f951246b1943391e2ec99c80dfdf4
michaelstsnow <michael.st.snow@gmail.com>
2018-11-06 19:00:27 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,24 +2,16 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       :patriarch =>{
-        :name=>""
-        :age=>""
       },
       :matriarch =>{
-        :name=>""
-        :age=>""
       },
       :hero=>{},
       :hero_friends=>[]
       },
     :capulet => {
       :patriarch =>{
-        :name=>""
-        :age=>""
       },
       :matriarch =>{
-        :name=>""
-        :age=>""
       },
       :heroine =>{},
       :heroine_friends =>[],
