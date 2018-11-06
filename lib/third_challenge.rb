b3fd8c622d5c8881990ce04df6f8a3200e85575e
michaelstsnow <michael.st.snow@gmail.com>
2018-11-06 19:03:29 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -1,29 +1,6 @@
-def second_challenge
+def first_challenge
   epic_tragedy = {
-    :montague => {
-      :patriarch => {
-        :name=>"",
-        :age=>""
-      },
-      :matriarch =>{
-        :name=>"",
-        :age=>""
-      },
-      :hero=>{},
-      :hero_friends=>[]
-      },
-    :capulet => {
-      :patriarch =>{
-        :name=>"",
-        :age=>""
-      },
-      :matriarch =>{
-        :name=>"",
-        :age=>""
-      },
-      :heroine =>{},
-      :heroine_friends =>[],
-      }}
-
-
+    :montague => {},
+    :capulet => {}
+  }
 end
